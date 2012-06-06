2674d7a13e50f09d99a018ac9e6eeb1668c5faec
Juan J. Garcia <juanjo.garcia@gmail.com>
2012-06-06 21:16:10 +0000

    Initial commit.
    git-svn-id: https://www.sltech-gt.com/svn/solylunaweb/trunk@3 66c18570-f49a-42cf-93d3-b93f38a46dba

---
+++ src/solylunaweb/application/controllers/site.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ *  Copyright (C) 2011 - 2012 SL Tech, S.A.
+ *  Juan J. Garcia
+ *  sltech.gt@gmail.com
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  
+ */
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Site extends CI_Controller {
+
+  /**
+      * Index Page for this controller.
+      *
+      * Maps to the following URL
+      * 		http://example.com/index.php/welcome
+      *	- or -  
+      * 		http://example.com/index.php/welcome/index
+      *	- or -
+      * Since this controller is set as the default controller in 
+      * config/routes.php, it's displayed at http://example.com/
+      *
+      * So any other public methods not prefixed with an underscore will
+      * map to /index.php/welcome/<method_name>
+      * @see http://codeigniter.com/user_guide/general/urls.html
+      */
+  public function index()
+  {
+    $this->load->view('ws_view');
+  }
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ src/solylunaweb/application/views/ws_view.php
@@ -0,0 +1,224 @@
+<?php
+/*
+ *  Copyright (C) 2011 - 2012 SL Tech, S.A.
+ *  Juan J. Garcia
+ *  sltech.gt@gmail.com
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Bootstrap, from Twitter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="<?= base_url('/assets/bootstrap/css'); ?>/bootstrap.css" rel="stylesheet">  
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+    <link href="<?= base_url('/assets/bootstrap/css'); ?>/bootstrap-responsive.css" rel="stylesheet">
+    
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="<?= base_url('/assets/images'); ?>/SL_favicon_32x32.png">
+    
+    <!-- Le javascript
+    ================================================== -->
+    <script src="<?= base_url('/assets/js/jquery-1.7.2.js'); ?>"></script>
+    <script src="<?= base_url('/assets/prettify/prettify.js'); ?>"></script>
+    <script src="<?= base_url('/assets/bootstrap/js/bootstrap.js'); ?>"></script>
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#">Project name</a>
+          <div class="nav-collapse">
+            <ul class="nav">
+              <li class="active"><a href="#"><i class="icon-home icon-white"></i> Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+            <form class="navbar-search pull-right">
+              <input type="text" class="search-query" placeholder="Search" />
+            </form>
+            <ul class="nav pull-right">
+              <li class="active"><a href="#">TEST</a></li>
+
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li class="nav-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>              
+            </ul>
+            
+          </div><!--/.nav-collapse -->
+        </div>
+      </div> 
+    </div>
+    
+    <div class="container-fluid">
+      <div class="row-fluid">
+        <div class="span2">
+        <!--Sidebar content-->
+          <div class="well sidebar-nav">
+            <ul class="nav nav-list">
+              <li class="nav-header">Sidebar</li>
+              <li class="active"><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li class="nav-header">Sidebar</li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li class="nav-header">Sidebar</li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+            </ul>
+          </div><!--/.well -->
+
+        </div>
+        <div class="span10">
+        <!--Body content-->
+          <div class="hero-unit">
+            <h1>Hello, world!</h1>
+            <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+            <p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>
+          </div>
+          
+          <div class="row-fluid">
+            <div class="span2">
+              &nbsp;
+            </div>
+            <div class="span8">
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn" href="#">View details &raquo;</a></p>
+            </div><!--/span-->        
+            <div class="span2">
+              &nbsp;
+            </div>
+          </div>
+          <div class="row-fluid">
+            <div class="span4">
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn" href="#">View details &raquo;</a></p>
+            </div><!--/span-->        
+            <div class="span4">
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn" href="#">View details &raquo;</a></p>
+            </div><!--/span-->        
+            <div class="span4">
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn" href="#">View details &raquo;</a></p>
+            </div><!--/span-->        
+          </div>
+        </div> <!-- span10 -->
+        
+        
+
+      </div> <!-- container -->
+    </div>
+    <div class="container" >
+      <footer>
+        <p>&copy; Company 2012</p>
+      </footer>
+    </div>
+    
+    
+    
+    
+    
+    
+    <div class="container" >
+
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="hero-unit">
+        <h1>Hello, world!</h1>
+        <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="span4">
+          <h2>Heading</h2>
+           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+        <div class="span4">
+          <h2>Heading</h2>
+           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+       </div>
+        <div class="span4">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+      </div>
+     
+
+<!-- *** FOOTER *** !--->
+      <hr />
+
+      <footer>
+        <p>&copy; Company 2012</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+   
+    <script>
+
+      // Activate Google Prettify for pretty-printing code
+      //addEventListener('load', prettyPrint, false);
+
+      $(document).ready(function() {
+        // Add prettyprint class to pre elements
+        $('pre').addClass('prettyprint');    
+        $('.dropdown-toggle').dropdown();
+      });
+
+    </script> 
+
+  </body>
+</html>
